99f7721c0b661dfc85c5bbcfa1375b44ec8816b8
Pedro <pedrolnarciso@gmail.com>
2023-08-01 11:11:03 -0300

    triste

---
+++ pw3viagem/src/main/java/br/com/etechoracio/pw3viagem/Pw3viagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.pw3viagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3viagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3viagemApplication.class, args);
+	}
+
+}
---
+++ pw3viagem/src/test/java/br/com/etechoracio/pw3viagem/Pw3viagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.pw3viagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3viagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
